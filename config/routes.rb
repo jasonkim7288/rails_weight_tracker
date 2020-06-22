054512d986ad1c67baed7439a7eb04dd029634fa
jasonkim7288 <jason.kim7288@gmail.com>
2020-06-22 15:53:54 +1000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'weights/index'
+  get 'weights/show'
+  get 'weights/new'
+  get 'weights/create'
+  get 'weights/update'
+  get 'weights/edit'
+  get 'weights/destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
---
+++ test/controllers/weights_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class WeightsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get weights_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get weights_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get weights_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get weights_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get weights_update_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get weights_edit_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get weights_destroy_url
+    assert_response :success
+  end
+
+end
