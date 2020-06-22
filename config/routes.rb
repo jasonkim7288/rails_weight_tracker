712e10162a99c19ed77a370140a0c0fa0d2c667e
jasonkim7288 <jason.kim7288@gmail.com>
2020-06-22 16:19:24 +1000

    modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'weights/index'
-  get 'weights/show'
-  get 'weights/new'
-  get 'weights/create'
-  get 'weights/update'
-  get 'weights/edit'
-  get 'weights/destroy'
+  get 'weights', to: "weights#index", as: "weights"
+  get 'weights/new', to: "weights#new", as: "new_weight"
+  get 'weights/:id', to: "weights#show", as: "weight"
+  post 'weights', to: "weight#create"
+  put 'weights/:id', to: "weight#update"
+  get 'weights/:id/edit', to: "weight#edit", as: "edit_weight"
+  delete 'weights/:id', to: "weight#destroy"
+
+  root "weights#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
